a2c184c3b78d06a0b8c3cd834ada5ad777fea3ed
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-19 15:34:15 -0500

    first

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,8 @@
--- Database name
-
+-- Database name treats
+CREATE TABLE treat (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(64),
+  description TEXT,
+  pic VARCHAR(200)
+);
 -- Document you create tables pSQL here
