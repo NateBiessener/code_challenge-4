8e8da2dbdb1f0299724216160eafae9313b7a616
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-19 16:32:54 -0500

    update get route to handle queries

--- server/app.js
+++ server/app.js
@@ -24,17 +24,24 @@ app.get('/treats', urlencodedParser, function(req, res){
       console.log(err);
     }
     else {
+      console.log('in /treats get, successful connect, query:', req.query.q);
       var resultArray = [];
-      var queryResult = client.query('SELECT * FROM treat');
+      var queryString = 'SELECT * FROM treat';
+      //if get included query, add get query to sql query
+      if (req.query.q){
+        queryString += ' WHERE name LIKE \'%' + req.query.q.toLowerCase() + '%\'';
+        console.log(queryString);
+      }//end if
+      var queryResult = client.query(queryString);
       queryResult.on('row', function(row){
         resultArray.push(row);
-      });
+      });//end on row
       queryResult.on('end', function(){
         done();
         res.send(resultArray);
-      });
-    }
-  });
+      });//end end
+    }//end else
+  });//end connect
 });//end /treats get
 
 /*receives {
@@ -49,11 +56,11 @@ app.post('/treats', urlencodedParser, function(req, res){
       console.log(err);
     }
     else {
-      client.query('INSERT INTO treat (name, description, pic) VALUES ($1, $2, $3)', [req.body.name, req.body.description, req.body.url]);
+      client.query('INSERT INTO treat (name, description, pic) VALUES ($1, $2, $3)', [req.body.name.toLowerCase(), req.body.description, req.body.url]);
       done();
       res.send(true);
-    }
-  });
-});
+    }//end else
+  });//end connect
+});//end /treast post
 
 app.use(express.static('public'));
